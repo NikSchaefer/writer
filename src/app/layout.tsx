adc252eace5977d1a0af0f0ffa9d561fab478049
Nik Schaefer <nikkschaefer@gmail.com>
2025-05-20 22:38:54 -0500

    chore: update dependencies, remove unused font files, and switch to Google Fonts for improved performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import { sohne } from "./fonts";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Writer",
@@ -12,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${sohne.variable} antialiased`}>
-      <body className={`${sohne.className}`}>{children}</body>
+    <html lang="en" className={`${inter.className} antialiased`}>
+      <body>{children}</body>
     </html>
   );
 }
